c6e4b8f772a041f7e29f50299bd8cc2ff57171da
fkerbey <nsaidou19744r@126.com>
2021-04-17 17:17:29 +0800

    fix

--- src/Client.java
+++ src/Client.java
@@ -1,21 +1,163 @@
-import java.io.*;
+﻿import java.io.*;
 import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
 import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Timer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
 /**
  * Created by lylw on 2017/7/17.
  */
 public class Client {
+
+    private static Long timeInterval;
+    private static Long startPoint;
+    private static boolean _switch;
+
+    public static void setTimeInterval(Long timeInterval) {
+        Client.timeInterval = timeInterval;
+    }
+
+    public static void setStartPoint(Long startPoint) {
+        Client.startPoint = startPoint;
+    }
+
+    public static void set_switch(boolean _switch) {
+        Client._switch = _switch;
+    }
+
     public static void main(String[] args) throws IOException {
-        writeFilesToServer("G:\\testFile");
+        SettingThread settingThread=new SettingThread();
+        settingThread.start();
+        TimingThread timingThread=new TimingThread(Long.valueOf(0),Long.valueOf(0),false);
+        timingThread.start();
+        TransferThread transferThread=new TransferThread();
+        transferThread.run();
+    }
+    private static class TimingThread extends Thread{
+        public TimingThread(Long startPoint,Long timeInterval,boolean _switch){
+            Client.startPoint=startPoint;
+            Client.timeInterval=timeInterval;
+            Client._switch=_switch;
+        }
+        public void run(){
+            Timer timer=new Timer();
+            timer.schedule(new TransferThread(),1000L,60000L);
+        }
+    }
+    private static class SettingThread extends Thread{
+        public void run(){
+            while(true){
+                Scanner in = new Scanner(System.in);
+                String input=in.nextLine();
+            }
+        }
+    }
+    private static class TransferThread extends java.util.TimerTask{
+        public TransferThread(){
+
+        }
+        public void run() {
+            try {
+                writeFilesToServer("G:\\testFile");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private static class MyThread extends Thread {
+        private Socket socket;
+        private String absolutePath;
+        private String MD5;
+        private Long bytePosition;
+        public MyThread(Socket socket, String absolutePath,Long bytePosition) {
+            this.socket = socket;
+            this.absolutePath = absolutePath.substring(0, absolutePath.length());
+            this.bytePosition=bytePosition;
+        }
+
+        public void run() {
+            try {
+                sendFileNameAndLength(absolutePath);
+                InputStream ins = socket.getInputStream();
+                byte[] input = new byte[1024];
+                ins.read(input);
+                boolean t = writeFileToServer(absolutePath);
+                System.out.println("finish send file\n");
+                ins.read(input);
+                System.out.println(MD5+" "+new String(input)+"\n");
+                if (t) {
+                    //deleteFile(file2.getAbsolutePath());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private boolean sendFileNameAndLength(String absolutePath) throws IOException {
+            boolean t = true;
+            File file = new File(absolutePath);
+            OutputStream os = socket.getOutputStream();
+            PrintWriter pw = new PrintWriter(os);
+            pw.write(absolutePath + " " + file.length() + " "+bytePosition+" ");
+            pw.flush();
+            os.flush();
+            return t;
+        }
+
+        private boolean writeFileToServer(String absolutePath) throws IOException, NoSuchAlgorithmException {
+            boolean t = true;
+            MD5=Md5CaculateUtil.getFileMD5(absolutePath);
+            OutputStream os = socket.getOutputStream();
+            File file = new File(absolutePath);
+            FileInputStream in = new FileInputStream(file);
+            byte[] buffer = new byte[16];
+            int size = 0;
+            bytePosition= Long.valueOf(0);
+            while ((size = in.read(buffer)) != -1) {
+                System.out.println("客户端发送数据包，大小为" + size);
+                os.write(buffer, 0, size);
+                os.flush();
+                InputStream ins = socket.getInputStream();
+                byte[] readAccept=new byte[16];
+                ins.read(readAccept);
+                String temp=new String(readAccept);
+                System.out.println("temp "+temp.split("\n")[0]);
+                bytePosition+=Long.parseLong(temp.split("\n")[0]);
+                System.out.println("bytePosition "+bytePosition);
+            }
+            //os.close();
+            //socket.close();
+            //in.close();
+            return t;
+        }
+        private static boolean deleteFile(String absolutePath) {
+            boolean t = false;
+            File file = new File(absolutePath);
+            if (file.exists() && file.isFile()) {
+                if (file.delete()) {
+                    System.out.println("删除单个文件" + absolutePath + "成功！");
+                    t = true;
+                } else {
+                    System.out.println("删除单个文件" + absolutePath + "失败！");
+                    t = false;
+                }
+            } else {
+                System.out.println("删除单个文件失败：" + absolutePath + "不存在！");
+                t = false;
+            }
+            return t;
+        }
     }
 
     public static void writeFilesToServer(String path) throws IOException {
+        int port = 10086;
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
         int fileNum = 0, folderNum = 0;
         File file = new File(path);
-        ExecutorService fixedThreadPool= Executors.newFixedThreadPool(3);
         if (file.exists()) {
             LinkedList<File> list = new LinkedList<File>();
             File[] files = file.listFiles();
@@ -26,89 +168,30 @@ public class Client {
                     folderNum++;
                 } else {
                     System.out.println("文件:" + file2.getAbsolutePath());
-                    fixedThreadPool.execute(new Runnable() {
-                        public void run(){
-                            try{
-                                boolean t=writeFileToServer(file2.getAbsolutePath());
-                                if(t){
-                                    //deleteFile(file2.getAbsolutePath());
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-
+                    Socket socket = new Socket("localhost", 10086);//1024-65535的某个端口
+                    fixedThreadPool.submit(new MyThread(socket, file2.getAbsolutePath(), (long) 0));
                     fileNum++;
                 }
             }
+            fixedThreadPool.shutdown();
         }
-        fixedThreadPool.shutdown();
     }
 
-    private static boolean deleteFile(String absolutePath) {
-        boolean t=false;
-        File file=new File(absolutePath);
-        if (file.exists() && file.isFile()) {
-            if (file.delete()) {
-                System.out.println("删除单个文件" + absolutePath + "成功！");
-                t=true;
-            } else {
-                System.out.println("删除单个文件" + absolutePath + "失败！");
-                t=false;
-            }
-        } else {
-            System.out.println("删除单个文件失败：" + absolutePath + "不存在！");
-            t=false;
-        }
-        return t;
-    }
-
-    public static boolean writeFileToServer(String path) throws IOException {
-
-        boolean t=true;
-        //调用accept()方法开始监听，等待客户端的连接
-        Socket socket = new Socket("localhost",10086);//1024-65535的某个端口
-
-        OutputStream os = socket.getOutputStream();
-
-        File file=new File(path);
-        FileInputStream in=new FileInputStream(file);
-        byte[] buffer = new byte[4096 * 5];
-        int size=0;
-        while((size = in.read(buffer)) != -1){
-            System.out.println("客户端发送数据包，大小为" + size);
-            /**向输出流中写入刚刚读到的数据包*/
-            os.write(buffer, 0, size);
-            /**刷新一下*/
-            os.flush();
-        }
-
-
-        //关闭资源
-        os.close();
-        socket.close();
-        in.close();
-        return t;
-    }
     public void readFromServer() throws IOException {
-        //调用accept()方法开始监听，等待服务端的连接
-        Socket socket = new Socket("localhost",10086);
-        //获取输入流，并读取服务端端信息
+        Socket socket = new Socket("localhost", 10086);
         InputStream is = socket.getInputStream();
-        InputStreamReader isr =new InputStreamReader(is);
-        BufferedReader br =new BufferedReader(isr);
-        String info =null;
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String info = null;
 
-        while((info=br.readLine())!=null){
-            System.out.println("Hello,我是客户端，服务器说："+info);
+        while ((info = br.readLine()) != null) {
+            System.out.println("Hello,我是客户端，服务器说：" + info);
         }
-        socket.shutdownInput();//关闭输入流
-        //获取输出流，响应服务器的请求
+        socket.shutdownInput();
         socket.close();
         br.close();
         isr.close();
         is.close();
-
     }
+
 }
--- src/Server.java
+++ src/Server.java
@@ -1,121 +1,189 @@
-import java.io.*;
+﻿import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.LinkedList;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
 /**
  * Created by lylw on 2017/7/17.
  */
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
-        int count=0;
-        //1、创建一个服务器端Socket，即ServerSocket，指定绑定的端口，并监听此端口
-        ServerSocket serverSocket =new ServerSocket(10086);//1024-65535的某个端口
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int port = 10086;
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         while(true){
-            fixedThreadPool.execute(new Runnable() {
-                public void run() {
-                    try {
-                        readFromClient(serverSocket);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
+            Socket socket = serverSocket.accept();
+            fixedThreadPool.submit(new MyThread(socket));
         }
     }
-    public static void writeFilesToClient(String path) throws IOException {
-        int fileNum = 0, folderNum = 0;
-        File file = new File(path);
-        if (file.exists()) {
-            LinkedList<File> list = new LinkedList<File>();
-            File[] files = file.listFiles();
-            for (File file2 : files) {
-                if (file2.isDirectory()) {
-                    System.out.println("文件夹:" + file2.getAbsolutePath());
-                    list.add(file2);
-                    folderNum++;
-                } else {
-                    System.out.println("文件:" + file2.getAbsolutePath());
-                    boolean t=writeFileToClient(file2.getAbsolutePath());
-                    if(t){
-                        deleteFile(file2.getAbsolutePath());
-                    }
-                    fileNum++;
-                }
+    private static class MyThread extends Thread{
+        private Socket socket;
+        private String receive_filePath;
+        private int fileSize;
+
+        public MyThread(Socket socket){
+            this.socket = socket;
+        }
+        /**
+         * write data for communication with client
+         */
+        public void run(){
+            try {
+                read(socket);
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
             }
         }
-    }
+        private void read(Socket socket) throws IOException, NoSuchAlgorithmException {
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr =new InputStreamReader(is);
+            BufferedReader br =new BufferedReader(isr);
+            byte[] buffer = new byte[128];
+            is.read(buffer);
 
-    private static boolean deleteFile(String absolutePath) {
-        boolean t=false;
-        File file=new File(absolutePath);
-        if (file.exists() && file.isFile()) {
-            if (file.delete()) {
-                System.out.println("删除单个文件" + absolutePath + "成功！");
-                t=true;
+            String info =new String(buffer);
+            String[] args=info.split(" ");
+
+            String path = null;
+            String base = "G:/receiveFile/";
+            path = args[0];
+            fileSize=Integer.parseInt(args[1].substring(0,args[1].length()));
+            String[] args1 = path.split("\\\\");
+            String fileName = args1[args1.length - 1];
+
+            String temp= base.concat(fileName);
+            receive_filePath=temp.substring(0,temp.length());
+            Long startPosition= Long.parseLong(args[2]);
+            System.out.println("startPosition");
+            File receive_file=new File(receive_filePath);
+            if(!receive_file.exists()){
+                receive_file.createNewFile();
+            }
+            FileInputStream fis=new FileInputStream(receive_file);
+            File temp_file=new File(base+"temp_"+fileName);
+
+            byte[] copyfile=new byte[16];
+            FileOutputStream fos= new FileOutputStream(temp_file);
+
+            int read=0;
+            int total_read=0;
+            while(total_read<startPosition){
+                read=fis.read(copyfile);
+                fos.write(copyfile);
+                total_read+=read;
+            }
+            fos.close();
+            fis.close();
+            int tempsize=0;
+            fis=new FileInputStream(temp_file);
+            fos=new FileOutputStream(receive_file);
+            while(tempsize<startPosition){
+                tempsize+=fis.read(copyfile);
+                fos.write(copyfile);
+            }
+            fis.close();
+            if(!temp_file.delete()){
+                System.out.println("delete file "+temp_file.getAbsoluteFile()+" fail");
+            }
+            OutputStream ous=socket.getOutputStream();
+            PrintWriter pw=new PrintWriter(ous);
+            pw.write("ok\n");
+            pw.flush();
+            ous.flush();
+
+            int receive_size=0;
+            int readSize=0;
+            while((receive_size<fileSize) && ((readSize=is.read(buffer))!=-1)){
+                receive_size+=readSize;
+                System.out.println("receive_size "+receive_size);
+                fos.write(buffer,0,readSize);
+                pw.write(readSize+"\n");
+                pw.flush();
+                ous.flush();
+            }
+            String md5=Md5CaculateUtil.getFileMD5(receive_filePath);
+            pw.write(md5+"\n");
+            pw.flush();
+            ous.flush();
+            fos.close();
+            br.close();
+            isr.close();
+            is.close();
+        }
+
+        private boolean readFileNameAndSize(Socket socket) throws IOException {
+            boolean t=true;
+            InputStream is = socket.getInputStream();
+            InputStreamReader isr =new InputStreamReader(is);
+            BufferedReader br =new BufferedReader(isr);
+            String info =br.readLine();
+            String[] args=info.split(" ");
+            String path = null;
+            String base = "G:/receiveFile/";
+            path = args[0];
+            fileSize=95;
+            String[] args1 = path.split("\\\\");
+            String fileName = args1[args1.length - 1];
+            //System.out.println("fileName "+fileName);
+            String temp= base.concat(fileName);
+            receive_filePath=temp.substring(0,temp.length());
+            //System.out.println("Hello,我是服务器，客户端说：" + info.split(" ")[0]+"\n");
+            System.out.println("filePath " + receive_filePath + "\n");
+            br.close();
+            isr.close();
+            //is.close();
+            return t;
+        }
+
+        public void readFromClient(Socket socket) throws IOException {
+            InputStream is = socket.getInputStream();
+            System.out.println("receice_filePath "+receive_filePath+" InputStream Size "+is.available());
+            InputStreamReader isr =new InputStreamReader(is);
+            BufferedReader br =new BufferedReader(isr);
+            FileOutputStream out=new FileOutputStream(receive_filePath);
+            //byte[] buffer = new byte[4096 * 5];
+            byte[] buffer = new byte[128];
+            int receive_size=0;
+            int readSize=0;
+            DataInputStream dis = new DataInputStream(new BufferedInputStream(is));
+            while((readSize=dis.read(buffer))!=-1){
+                receive_size+=readSize;
+                out.write(buffer,0,readSize);
+            }
+            out.close();
+            socket.shutdownInput();
+            br.close();
+            isr.close();
+            //is.close();
+            //socket.close();
+        }
+        private static boolean deleteFile(String absolutePath) {
+            boolean t=false;
+            File file=new File(absolutePath);
+            if (file.exists() && file.isFile()) {
+                if (file.delete()) {
+                    System.out.println("删除单个文件" + absolutePath + "成功！");
+                    t=true;
+                } else {
+                    System.out.println("删除单个文件" + absolutePath + "失败！");
+                    t=false;
+                }
             } else {
-                System.out.println("删除单个文件" + absolutePath + "失败！");
+                System.out.println("删除单个文件失败：" + absolutePath + "不存在！");
                 t=false;
             }
-        } else {
-            System.out.println("删除单个文件失败：" + absolutePath + "不存在！");
-            t=false;
+            return t;
         }
-        return t;
-    }
-
-    public static boolean writeFileToClient(String path) throws IOException {
-        /**
-         * 基于TCP协议的Socket通信，实现用户登录，服务端
-         */
-        boolean t=true;
-        //1、创建一个服务器端Socket，即ServerSocket，指定绑定的端口，并监听此端口
-        ServerSocket serverSocket =new ServerSocket(10086);//1024-65535的某个端口
-        //2、调用accept()方法开始监听，等待客户端的连接
-        Socket socket = serverSocket.accept();
-
-        OutputStream os = socket.getOutputStream();
-        File file=new File(path);
-        FileInputStream in=new FileInputStream(file);
-        byte[] buffer = new byte[4096 * 5];
-        int size=0;
-        while((size = in.read(buffer)) != -1){
-            System.out.println("服务器发送数据包，大小为" + size);
-            /**向输出流中写入刚刚读到的数据包*/
-            os.write(buffer, 0, size);
-            /**刷新一下*/
-            os.flush();
-        }
-
-
-//5、关闭资源
-        os.close();
-        socket.close();
-        serverSocket.close();
-
-        return t;
-    }
-    public static void readFromClient(ServerSocket serverSocket) throws IOException {
-
-        //2、调用accept()方法开始监听，等待客户端的连接
-        Socket socket = serverSocket.accept();
-        //3、获取输入流，并读取客户端信息
-        InputStream is = socket.getInputStream();
-        InputStreamReader isr =new InputStreamReader(is);
-        BufferedReader br =new BufferedReader(isr);
-        String info =null;
-
-        while((info=br.readLine())!=null){
-            System.out.println("Hello,我是服务器，客户端说："+info);
-        }
-        socket.shutdownInput();//关闭输入流
-        // 4、获取输出流，响应客户端的请求
-
-        br.close();
-        isr.close();
-        is.close();
-        socket.close();
     }
 }
